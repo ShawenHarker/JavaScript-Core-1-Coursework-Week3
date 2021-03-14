ae274b378fe1190ca53c073aaa74843471b6ac0b
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-03-14 15:37:33 +0200

    mandatory 6th
    Completed the 6th exercise.

--- 2-mandatory/6-lane-names.js
+++ 2-mandatory/6-lane-names.js
@@ -6,7 +6,10 @@
   HINT: string and array methods that could be helpful (indexOf, filter)
 */
 
-function getLanes() {}
+function getLanes(arr) {
+  return arr.filter((el) => el.includes("Lane"))
+  .map((el) => el)
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
